092124df7c657d8a8f79a45a906acfccf03397a4
Killer9000 <33374800+Killer9000@users.noreply.github.com>
2020-02-14 21:56:52 +0000

    Scripts/Exceptions: Basic script to handle exception writing, needs to be changed up since it doesn't fit with the unity style.
    Scripts/Levels: The base of the level system, mostly ready to go, but needs to be finished.
    
    Scripts/

---
+++ Assets/Scripts/Exceptions/ExceptionWriting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Exceptions
+{
+    public static class ExceptionWriting
+    {
+        public static void WriteException(Exception ex)
+        {
+            string exceptionMsg = "-- Message\n" +
+                $"{ex.Message}\n" +
+                $"-- Entire Message -----------\n" +
+                $"{ex}\n";
+
+            Debug.LogError(exceptionMsg);
+        }
+    }
+}
---
+++ Assets/Scripts/Levels/LevelBase.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace LevelSystem
+{
+    public abstract class LevelBase : MonoBehaviour
+    {
+        /// <summary>
+        /// This is called when the player teleports to the teleport point.
+        /// </summary>
+        public abstract void OnPlayerTeleport();
+
+        /// <summary>
+        /// This will be called when the level starts to load.
+        /// </summary>
+        public abstract void OnLoadLevel();
+
+        /// <summary>
+        /// Going to do some basic logic.
+        /// </summary>
+        public virtual void Start()
+        {
+            if (!this.GetComponent<TeleportPoint>())
+                Debug.LogError("\b<LevelSystem\b>This script is supposed to be placed onto a teleport point");
+        }
+
+        public virtual void Update()
+        {
+
+        }
+    }
+}
