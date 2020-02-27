4f786e607013f10ada151968c8ebd53608c3a586
Watchamacall <ryanpc123@gmail.com>
2020-02-27 10:41:02 +0000

    Scene: Added MainMenu.unity; Changed SampleScene.unity to Test.unity
    Scripts: Added LevelChange.cs
    
    Made MainMenu.unity build order 0
    Made Test.unity build order 0
    
    Added TextMesh Pro folder

---
+++ Assets/Scripts/Levels/LevelChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        LevelChanging();
+    }
+
+    void LevelChanging()
+    {
+        
+    }
+
+     void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Hand")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
